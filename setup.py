441a9a02791fd76f763b5092ee6717256d56018e
Paul Branson <paul.branson@csiro.au>
2019-05-17 18:07:47 +1000

    removed requirements

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open('README.rst') as readme_file:
 with open('HISTORY.rst') as history_file:
     history = history_file.read()
 
-requirements = ['numpy', 'scipy', 'shapely', 'xarray', 'matplotlib', 'gdal', 'osr', 'pyproj', 'cartopy', 'pandas']
+requirements = [ ]
 
 setup_requirements = [ ]
 
