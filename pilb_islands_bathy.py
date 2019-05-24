4f0208b2c21b79e580c9f9829f339d06ec76e1ab
mcuttler <michael.cuttler@uwa.edu.au>
2019-05-24 09:41:49 +0800

    create file for pilbara islands

---
+++ pilb_islands_bathy.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Testing dembuilder for Pilbara Islands bathymetry
+
+M Cuttler
+May 2019
+
+"""
+
+#import python modules
+import sys
+import numpy as np
+
+#import p branson modules
+import dembuilder as db
+
+sys.path.append('../')
